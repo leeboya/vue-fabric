95aae07b6857bacd8883d96bfc0829a2196da203
afacode <afacode@outlook.com>
2018-03-06 18:10:15 +0800

    左侧拖拽测试

--- src/views/drag.vue
+++ src/views/drag.vue
@@ -1,25 +1,21 @@
 <template>
-  <div class="m-lfbar">
+  <div class="contain">
     <div class="leftBar">
       <img :src="imgInstance[0].pic" draggable="true" id="dragImg" @dragstart="dragstart($event)" @dragend="dragend($event)" />
     </div>
-    <!-- <div class="rigth-canvas">
-      <canvas class="canvas"  width="600" id="canvas"  height="600" @drop="drop($event)" ></canvas>
-    </div> -->
+    <!-- <div class="rigth-canvas"> -->
+    
+      <canvas class="canvas"  width="600" id="canvas"  height="600" @drop="drop($event)" @dragover="dragover($event)" ></canvas>
+    <!-- </div> -->
   </div>
+  
 </template>
-
 <style scoped>
 .contain{
   display: flex;
 }
-.m-lfbar {
- height: 1000px;
- border:#eee 1px solid;
- padding:10px;
-}
 .leftBar{
-  width: 200px;
+  width: 400px;
   height: 600px;
   /* background:#ccc; */
   border:1px solid red;
@@ -40,7 +36,7 @@ export default {
           position: { left: 100, top: 100, width: 200, height: 198, angle: 10 }
         }
       ],
-      canvas: new fabric.Canvas('canvas')
+      canvas:null
     };
   },
   mounted() {
@@ -49,49 +45,51 @@ export default {
     
   },
   created() {
-    // var canvas = new fabric.Canvas('canvas');
+      this.updateImg();
   },
-  methods: { 
+  methods: {
+    updateImg(){
+      this.canvas = new fabric.Canvas('canvas');
+    },
     dragstart(ev){
       /*拖拽开始*/
-      console.log('鼠标拖拽点在图片的位置');
-      
+      // console.log('鼠标拖拽点在图片的位置');
       ev.dataTransfer.setData("url",ev.target.src);
-      let url = ev.dataTransfer.getData("url");
-      // console.log(url)
-      // var canvas = new fabric.Canvas('canvas');
+      // let url = ev.dataTransfer.getData("url");
     },
     dragend(ev){
       /*拖拽结束*/
       console.log('我放下图片了');
-      console.log(ev.target.style.width);
-      let url = ev.path[0].currentSrc
+      let url = ev.path[0].currentSrc;
       // let url = ev.dataTransfer.getData("url");
-      console.log(ev.offsetX);
+      // console.log(ev.offsetX);
+      let canvas = this.canvas;
       // var canvas = new fabric.Canvas('canvas');
-      // this.$emit('childMethod','childParam'); 
-      //第一个参数名为调用的方法名，第二个参数为需要传递的参数
       if(ev.offsetX > 300){
-        fabric.Image.fromURL(url, function(oImg){
+        this.cover(url,ev, canvas);
+      }
+    },
+    cover(url,ev, canvas){
+      // var canvas = new fabric.Canvas('canvas');
+      // let _self = this;
+      // var  canvas = this.canvas;
+      console.log(canvas)
+      new fabric.Image.fromURL(url, function(oImg){
             oImg.left = 10;
             oImg.top = 10;
-            // oImg.width = 100
-            // oImg.height = 100
             oImg.scale(1);
             canvas.add(oImg);
         })
-          // this.cover(url, ev);
-      }
-    },
-    cover(url,ev){
-
-      console.log(url)
         
     },
     drop(ev){
-      // ev.preventDefault();
+      // console.log('拖拽元素进入目标元素头上，同时鼠标松开的时候');
+      ev.preventDefault();
+    },
+    dragover(ev){
+      // console.log('拖拽元素在目标元素头上移动的时候');
+      ev.preventDefault();
     }
   }
 };
 </script>
-
