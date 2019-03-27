cb6e1c99e4dd725a0493882f32220a4fcb1bb24f
afacode <afacode@outlook.com>
2019-03-27 21:49:54 +0800

    update config process

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"', //发布环境 
-  QINIU_IMG:'"https://img.wesetup.cn/"',//七牛图片
-  qiyukf:'"9d26aae940dc05e124af352aa9938321.js"'
+  QINIU_IMG:'',//七牛图片
+  qiyukf:''
 
 };
--- config/sim.env.js
+++ config/sim.env.js
@@ -2,6 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sim"', //发布环境
-  QINIU_IMG:'"https://img.wesetup.cn/"',//七牛图片
-  qiyukf:'"a6caa2ba23e0c81f8ff2fd291db0113f.js"'
+  QINIU_IMG:'',//七牛图片
+  qiyukf:''
 };
